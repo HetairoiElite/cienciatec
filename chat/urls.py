c23f461e60660eca6e8ee44642bdda4ac1b8ad4c
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2022-12-02 00:41:32 -0600

    endpoint de prueba borrado

--- chat/urls.py
+++ chat/urls.py
@@ -6,8 +6,7 @@ from .views import *
 urlpatterns = [
     # path("", views.index, name="index"),
     path('', ThreadListView.as_view(), name='list_threads'),
-     path('thread/<int:pk>/', ThreadDetailView.as_view(), name='detail_thread'),
+    path('thread/<int:pk>/', ThreadDetailView.as_view(), name='detail_thread'),
     path('thread/<int:pk>/add_message/', add_message, name='add_message'),
     path('thread/start/<str:username>/', start_thread, name='start_thread'),
-    path("<str:room_name>/", room, name="room"),
 ]
