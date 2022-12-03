0ec719a84f9669c3fb573291311342584067420f
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2022-12-03 06:39:42 -0600

    prueba de dashboard con notificaciones

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from notifications.signals import notify
+from django.contrib.auth.models import User
+from chat.models import Thread
 # Create your views here.
+
+
 def dashboard(request):
-    return render(request, 'dashboard/index.html')
+    sender = Thread.objects.get(id=1)
+    recipient = User.objects.get(id=2)
+    message = "This is an simple message"
+    notify.send(sender, recipient=recipient, verb='Message',
+                description=message)
+    context = {
+        'user': request.user
+    }
+
+    return render(request, 'dashboard/index.html', context)
