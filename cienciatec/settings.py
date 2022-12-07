8022245eed8881d02ea038f89643339792e6518b
HetairoiElite <jonathan90090@gmail.com>
2022-12-07 08:22:20 +0000

    cambio de storages

--- cienciatec/settings.py
+++ cienciatec/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'school',
     'notifications',
     'dashboard',
+    'storages',
 ]
 
 DJANGO_NOTIFICATIONS_CONFIG = {'USE_JSONFIELD': True}
@@ -146,18 +147,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATIC_ROOT = BASE_DIR / 'static'
-
-# * Media
-
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = BASE_DIR / 'media'
 
 # * Email
 
@@ -185,3 +175,47 @@ PASSWORD_RESET_TIMEOUT = 3600  # 1 hour
 
 SESSION_COOKIE_AGE = 3600  # 1 hour
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+# * AWS
+
+# * S3
+
+
+AWS_S3_ACCESS_KEY_ID = os.getenv('AWS_S3_ACCESS_KEY_ID')
+
+AWS_S3_SECRET_ACCESS_KEY = os.getenv('AWS_S3_SECRET_ACCESS_KEY')
+
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_DEFAULT_ACL = "public-read"
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+AWS_S3_FILE_OVERWRITE = True
+
+AWS_LOCATION = 'static'
+
+AWS_QUERYSTRING_AUTH = False
+
+AWS_HEADERS = {
+    "Access-Control-Allow-Origin": "*",
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+
+STATICFILES_STORAGE = 'core.storage_backends.StaticStorage'
+
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+
+PUBLIC_MEDIA_LOCATION = 'media'
+
+DEFAULT_FILE_STORAGE = 'core.storage_backends.PublicMediaStorage'
+
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
