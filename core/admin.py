7151e01065e7fdcd3a7b20bfd50eaa5f5468d0ad
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-01-01 01:43:13 -0600

    admin para la app home

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.utils.html import format_html
+# * models
+from core.models import Home
 
 # Register your models here.
+
+# * home admin
+
+
+class HomeAdmin(admin.ModelAdmin):
+    
+    # * deactive add button
+    def has_add_permission(self, request):
+        return False
+    
+    # * deactive delete button
+    def has_delete_permission(self, request, obj=None):
+        return False
+    
+
+    # * image preview
+    def image_preview(self, obj):
+        return format_html('<img src="/media/{}" width="200" />'.format(obj.image))
+    image_preview.short_description = 'Imagen principal'
+
+    # * brand image preview
+
+    def brand_image_preview(self, obj):
+        return format_html('<img src="/media/{}" width="200" />'.format(obj.brand_image))
+
+    brand_image_preview.short_description = 'Imagen de marca'
+
+    # * list display
+    list_display = ('title', 'image_preview', 'brand_image_preview')
+
+    fieldsets = (
+        ('Página principal', {
+         'fields': ('title', 'subtitle', 'description', 'image', 'brand_image')}),
+    )
+
+
+# * admin
+admin.site.register(Home, HomeAdmin)
