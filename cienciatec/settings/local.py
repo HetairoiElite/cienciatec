a98ba87a18e99e6c47e9fb0cbca1d073b5f07eac
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-01-01 02:45:33 -0600

    local database

--- cienciatec/settings/local.py
+++ cienciatec/settings/local.py
@@ -8,3 +8,16 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+# * Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
