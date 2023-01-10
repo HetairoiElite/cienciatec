100ebc7844858706b65fdea481eb21271cd9ab2f
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-01-10 07:45:38 -0600

    appname y vistas basadas en clases

--- apps/registration/urls.py
+++ apps/registration/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from .views import *
 
+app_name = 'registration'
+
 urlpatterns = [
-    path('signup/', SignUpView, name='signup'),
+    path('signup/', SignUp.as_view(), name='signup'),
     path('sent_email/', SentEmailView, name='sent_email'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('test_email/', test_email, name='test_email'),
     path('activate/<uidb64>/<token>/', activeEmail, name='activate'),
     path('email/resend_link/',
          new_link_active_email, name='resend_link_email'),
-    path('profile/', ProfileUpdateView, name='profile'),
+    path('profile/', UpdateProfile.as_view(), name='profile'),
 ]
