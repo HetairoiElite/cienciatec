bedb4f5494903d1116f47e56afb46905f8b115bc
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-01-17 16:55:53 -0600

    configuracion de app

---
+++ apps/proposal_reception/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ProposalReceptionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.proposal_reception'
+    verbose_name = 'Recepción de propuestas'
