9a651ae752a62bb37dbb5fa794ea594787eaaf6e
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-01-17 16:59:07 -0600

    custom upload y otros fields

--- core/models.py
+++ core/models.py
@@ -3,17 +3,25 @@ from django.dispatch import receiver
 from django.db.models.signals import post_delete
 from django.db import models
 
+# * mark safe
+from django.utils.safestring import mark_safe
+
 # * django model utils
 from model_utils.models import TimeStampedModel
 
 # Create your models here.
 
+
 # * custom upload to
 
+
 def custom_upload_to(instance, filename):
-    old_instance = Home.objects.get(pk=instance.pk)
-    old_instance.image.delete()
-    return 'home/'+filename
+    try:
+        old_instance = Home.objects.get(pk=instance.pk)
+        old_instance.image.delete()
+        return 'home/'+filename
+    except Home.DoesNotExist:
+        return 'home/'+filename
 
 
 # * home model
@@ -22,16 +30,21 @@ def custom_upload_to(instance, filename):
 class Home(TimeStampedModel):
     title = models.CharField(max_length=100, verbose_name='Título')
     subtitle = models.CharField(max_length=100, verbose_name='Subtítulo')
-    
+
     image = models.ImageField(
         verbose_name='Imagen Principal', upload_to=custom_upload_to, null=True, blank=True)
     brand_image = models.ImageField(
         verbose_name='Imagen de marca', upload_to=custom_upload_to, null=True, blank=True)
-    
+
     favicon = models.ImageField(
         verbose_name='Favicon', upload_to=custom_upload_to, null=True, blank=True)
-    
+
     description = models.TextField(verbose_name='Descripción')
+
+    # * pdf de la convocatoria
+    
+    convocatoria = models.FileField(
+        verbose_name='Convocatoria', upload_to=custom_upload_to, null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -40,6 +53,9 @@ class Home(TimeStampedModel):
         ordering = ['title']
         verbose_name = 'Página principal'
         verbose_name_plural = 'Página principal'
+
+    def image_previewc(self):
+        return mark_safe('<img src="%s" width="100px" />' % self.image.url)
 
 
 # * remove image file when object is deleted
@@ -50,5 +66,3 @@ def submission_delete(sender, instance, **kwargs):
     instance.image.delete(False)
     instance.brand_image.delete(False)
     instance.favicon.delete(False)
-
-    
