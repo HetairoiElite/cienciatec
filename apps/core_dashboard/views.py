c5fad16116cf84bc418af1931390bda9b27cf509
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-01-25 02:17:59 -0600

    view based class

--- apps/core_dashboard/views.py
+++ apps/core_dashboard/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from notifications.signals import notify
 from django.contrib.auth.models import User
+
+from django.views.generic import TemplateView
+
+# * login required mixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+# * apps
 from apps.chat.models import Thread
+
+
 # Create your views here.
 
 
-def dashboard(request):
-    
-    return render(request, 'core_dashboard/index.html')
+class Dashboard(LoginRequiredMixin, TemplateView):
+    template_name = 'core_dashboard/index.html'
+
+
+# class ProposalArticleDetail(LoginRequiredMixin, TemplateView):
+#     template_name = 'proposal_reception/article_proposal_detail.html'
