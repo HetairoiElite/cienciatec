054677dbe63def9dfa863ee26975495e7d6924a7
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-01-25 02:30:47 -0600

    publicacion actual en el contexto global

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,8 +1,14 @@
 # * context procesor for home
 
 from .models import Home
+from apps.events.models import Publication
+
 
 def home(request):
     home = Home.objects.first()
-    return {'home': home}
-
+    current_publication = Publication.objects.get_current()
+    context = {
+        'home': home,
+        'current_publication': current_publication,
+    }
+    return context
