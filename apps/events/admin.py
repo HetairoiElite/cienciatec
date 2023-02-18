c7bf43be7c75a3bc798c6858902d441e9a26a22c
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-02-18 13:21:56 -0600

    excepcion calendario

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -103,68 +103,72 @@ class PublicationAdmin(admin.ModelAdmin):
         next_month = datetime.date(year=next_month.year, month=next_month.month,
                                    day=1)  # find first day of next month
 
-        extra_context['previous_month'] = reverse('admin:events_publication_changelist') + '?start_date__gte=' + str(
-            previous_month)
-        extra_context['next_month'] = reverse(
-            'admin:events_publication_changelist') + '?start_date__gte=' + str(next_month)
+        try:
+            extra_context['previous_month'] = reverse('admin:Eventos_publication_changelist') + '?start_date__gte=' + str(
+                previous_month)
+            extra_context['next_month'] = reverse(
+                'admin:Eventos_publication_changelist') + '?start_date__gte=' + str(next_month)
 
-        cal = PublicationCalendar()
-        html_calendar = cal.formatmonth(d.year, d.month, withyear=True)
-        html_calendar = html_calendar.replace(
-            '<td ', '<td  width="200" height="150"')
-        html_calendar = html_calendar.replace(
-            'January', 'Enero'
-        ).replace(
-            'February', 'Febrero'
-        ).replace(
-            'March', 'Marzo'
-        ).replace(
-            'April', 'Abril'
-        ).replace(
-            'May', 'Mayo'
-        ).replace(
-            'June', 'Junio'
-        ).replace(
-            'July', 'Julio'
-        ).replace(
-            'August', 'Agosto'
-        ).replace(
-            'September', 'Septiembre'
-        ).replace(
-            'October', 'Octubre'
-        ).replace(
-            'November', 'Noviembre'
-        ).replace(
-            'December', 'Diciembre'
-        )
+            cal = PublicationCalendar()
+            html_calendar = cal.formatmonth(d.year, d.month, withyear=True)
+            html_calendar = html_calendar.replace(
+                '<td ', '<td  width="200" height="150"')
+            html_calendar = html_calendar.replace(
+                'January', 'Enero'
+            ).replace(
+                'February', 'Febrero'
+            ).replace(
+                'March', 'Marzo'
+            ).replace(
+                'April', 'Abril'
+            ).replace(
+                'May', 'Mayo'
+            ).replace(
+                'June', 'Junio'
+            ).replace(
+                'July', 'Julio'
+            ).replace(
+                'August', 'Agosto'
+            ).replace(
+                'September', 'Septiembre'
+            ).replace(
+                'October', 'Octubre'
+            ).replace(
+                'November', 'Noviembre'
+            ).replace(
+                'December', 'Diciembre'
+            )
 
-        # * traducir dias de la semana
+            # * traducir dias de la semana
 
-        html_calendar = html_calendar.replace(
-            'Mon', 'Lun'
-        ).replace(
-            'Tue', 'Mar'
-        ).replace(
-            'Wed', 'Mie'
-        ).replace(
-            'Thu', 'Jue'
-        ).replace(
-            'Fri', 'Vie'
-        ).replace(
-            'Sat', 'Sab'
-        ).replace(
-            'Sun', 'Dom'
-        )
-        extra_context['calendar'] = mark_safe(html_calendar)
-        return super(PublicationAdmin, self).changelist_view(request, extra_context)
-    
+            html_calendar = html_calendar.replace(
+                'Mon', 'Lun'
+            ).replace(
+                'Tue', 'Mar'
+            ).replace(
+                'Wed', 'Mie'
+            ).replace(
+                'Thu', 'Jue'
+            ).replace(
+                'Fri', 'Vie'
+            ).replace(
+                'Sat', 'Sab'
+            ).replace(
+                'Sun', 'Dom'
+            )
+            extra_context['calendar'] = mark_safe(html_calendar)
+            return super(PublicationAdmin, self).changelist_view(request, extra_context)
+        except:
+            return super(PublicationAdmin, self).changelist_view(request, extra_context)
+
     def message_user(self, *args, **kwargs):
-        pass 
-    
+        pass
+
     def save_model(self, request, obj, form, change):
         if change:
             messages.add_message(request, messages.SUCCESS,
                                  format_html('La <a href="{}">{}</a> ha sido actualizada correctamente.', reverse('admin:events_publication_change', args=[obj.id]), obj))
         super().save_model(request, obj, form, change)
+
 
 admin.site.register(Publication, PublicationAdmin)
