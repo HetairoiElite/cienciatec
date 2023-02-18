b77d031c63e5b1da6a9efa921e0e356dfdb5ce6f
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-02-18 13:26:07 -0600

    nueva label

--- apps/reviewer_assignment/apps.py
+++ apps/reviewer_assignment/apps.py
@@ -4,4 +4,5 @@ from django.apps import AppConfig
 class ReviewerAssignmentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.reviewer_assignment'
+    label = 'Asignacion_Arbitros'
     verbose_name = 'Asignación de arbitros'
