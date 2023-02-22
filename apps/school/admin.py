9838bf81799f80e1bda6349ac463dd8092a6fae0
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-02-22 02:59:48 -0600

    mensajes español

--- apps/school/admin.py
+++ apps/school/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import School
+from django.contrib import messages
 
 # Register your models here.
 
@@ -11,7 +12,28 @@ class SchoolAdmin(admin.ModelAdmin):
     search_fields = ('name',)
     list_filter = (
         'is_external',)
-    
+
+    def message_user(self, request, message, level=messages.INFO, extra_tags='', fail_silently=False):
+        pass
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            messages.success(
+                request, 'La escuela se ha actualizado correctamente')
+        else:
+            messages.success(request, 'La escuela se ha creado correctamente')
+
+        obj.save()
+
+    def delete_model(self, request, obj):
+        messages.success(request, 'La escuela se ha eliminado correctamente')
+        obj.delete()
+
+    # * delete action message
+    def delete_queryset(self, request, queryset):
+        messages.success(
+            request, 'Las escuelas se han eliminado correctamente')
+        queryset.delete()
 
 
 admin.site.register(School, SchoolAdmin)
