8873998bd46f7851d556fc8e468b48d5253194f1
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-02-22 02:49:27 -0600

    article review init

---
+++ apps/article_review/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ArticleReviewConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.article_review'
+    label = 'Revision_Articulo'
