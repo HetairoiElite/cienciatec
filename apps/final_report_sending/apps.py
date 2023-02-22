301ff65f857ccd7477957f382b9153993a42e20a
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-02-22 02:53:38 -0600

    final_report_sending

---
+++ apps/final_report_sending/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class FinalReportSendingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.final_report_sending'
+    label = 'Reporte_Final'
