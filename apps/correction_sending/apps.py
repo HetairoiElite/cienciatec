2e61b94f53baf291b8005132e18cf39bf91a4ecb
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-02-22 02:50:20 -0600

    correction_sending init

---
+++ apps/correction_sending/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class CorrectionSendingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.correction_sending'
+    label = 'Envio_Correcciones'
