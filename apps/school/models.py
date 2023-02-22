e6696fa0a229d173c15c41c876682822e3891a65
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-02-22 03:02:58 -0600

    not extra fields

--- apps/school/models.py
+++ apps/school/models.py
@@ -1,36 +1,16 @@
 from django.db import models
 from core import validators
 from .managers import SchoolManager
+from model_utils.models import TimeStampedModel
 # Create your models here.
 
 
-class School(models.Model):
+class School(TimeStampedModel):
     name = models.CharField(
         max_length=100, verbose_name='Nombre', validators=[validators.name])
-    address = models.CharField(
-        max_length=100, verbose_name='Dirección', null=True, blank=True)
-    city = models.CharField(
-        max_length=100, verbose_name='Ciudad', validators=[validators.name], null=True, blank=True)
-    state = models.CharField(
-        max_length=100, verbose_name='Estado', validators=[validators.name], null=True, blank=True)
-    zip_code = models.CharField(
-        max_length=100, verbose_name='Código postal', validators=[validators.zip_code], null=True, blank=True)
-    country = models.CharField(
-        max_length=100, verbose_name='País', validators=[validators.name], null=True, blank=True)
-    phone = models.CharField(
-        max_length=100, verbose_name='Teléfono', validators=[validators.phone], null=True, blank=True)
-    email = models.EmailField(
-        max_length=100, verbose_name='Correo electrónico', null=True, blank=True)
-    website = models.CharField(
-        max_length=100, verbose_name='Sitio web', validators=[validators.web_address], null=True, blank=True)
-    # * boolean external
+
     is_external = models.BooleanField(default=False, verbose_name='Externo')
 
-    created_at = models.DateTimeField(
-        auto_now_add=True, verbose_name='Creado en')
-    updated_at = models.DateTimeField(
-        auto_now=True, verbose_name='Actualizado en')
-    
     objects = SchoolManager()
 
     def __str__(self):
