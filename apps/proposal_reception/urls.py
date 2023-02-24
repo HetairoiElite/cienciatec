7ffa4173520f2a5742b77078b03f225264cf6ebd
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-02-24 02:49:37 -0600

    validator

--- apps/proposal_reception/urls.py
+++ apps/proposal_reception/urls.py
@@ -7,5 +7,6 @@ app_name = 'proposal_reception'
 urlpatterns = [
     path('crear/', views.ProposalFormView.as_view() , name='article_proposal_form'),
     path('<int:pk>/edit/', views.ArticleProposalUpdateView.as_view(), name='article_proposal_update'),
+    path('check-title/', views.CheckTitleView.as_view(), name='check_title'),
     # path('prueba/', views.prueba, name='prueba')
 ]
