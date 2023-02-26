87e0f45327dca6a7d67c41bbe4152757c32e44d9
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-02-26 17:59:38 -0600

    admin for article review

--- apps/article_review/admin.py
+++ apps/article_review/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+class NotesInline(admin.TabularInline):
+    model = Note
+    extra = 0
+    fk_name = 'review'
+
+
+class ArticleReviewAdmin(admin.ModelAdmin):
+
+    inlines = [NotesInline]
+
+    list_display = ('assignment',)
+    
+    readonly_fields = ('assignment',)
+
+
+admin.site.register(Review, ArticleReviewAdmin)
