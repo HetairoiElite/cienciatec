591eaf472067331dd1732024aef0cdc22d5fa35b
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-02-26 17:57:00 -0600

    max 2 arbitros max 4 asignaciones

--- apps/reviewer_assignment/forms.py
+++ apps/reviewer_assignment/forms.py
@@ -1,12 +1,14 @@
 
 from django import forms
+from django.db import models
 
 from .models import Assignment
 
 from apps.registration.models import Profile
 
+
 class AssignmentForm(forms.ModelForm):
-    
+
     class Meta:
         model = Assignment
         fields = [
@@ -14,21 +16,44 @@ class AssignmentForm(forms.ModelForm):
             'referees',
             'article',
         ]
-        
+
         widgets = {
-            'article': forms.Select(attrs={'class': 'form-control mb-2'}),
+            'article': forms.Select(),
         }
-        
+
         labels = {
             'referees': 'Arbitros',
             'article': 'Artículo',
         }
-        
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        print("hola")
-        # * concat queryset with option "Otra" with value "new"
-        self.fields['referees'].choices = [('', '---------')] + \
-            [(reviewer.id, reviewer.user.get_full_name())
-             for reviewer in Profile.objects.filter(type_user="2")]
+        # * máximo 4 asignaciones por arbitro y máximo 2 arbitros por artículo
+        
+        self.fields['referees'].queryset = Profile.objects.filter(
+            type_user="2",
+            profiles__in=self.instance.article.profile.profiles.all(),
+        ).distinct().annotate(
+            count_assignments=models.Count('assignments', filter=models.Q(assignments__completed=False))
+        ).filter(
+            count_assignments__lt=4
+        )
+        
+        a = 1
             
+    def clean(self):
+        
+        cleaned_data = super().clean()
+        
+        
+        referees = cleaned_data.get('referees')
+        
+        # * máximo 4 asignaciones por arbitro y máximo 2 arbitros por artículo
+        
+        if referees:
+            if len(referees) > 2:
+                self.add_error('referees', 'No puede asignar más de 2 arbitros')
+                
+        return cleaned_data
+
+        # * show obj
