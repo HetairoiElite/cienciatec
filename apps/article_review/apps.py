f571334f77d556fdaa5574d4e4100633b48b091b
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-02-26 17:59:54 -0600

    verbose name for app

--- apps/article_review/apps.py
+++ apps/article_review/apps.py
@@ -5,3 +5,4 @@ class ArticleReviewConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.article_review'
     label = 'Revision_Articulo'
+    verbose_name = 'Revisión de Artículos'
