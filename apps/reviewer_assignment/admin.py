b682bab3ccda5d3f6943c14ec75a429d6377af2d
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-02-26 17:54:49 -0600

    get profiles fields and built childs

--- apps/reviewer_assignment/admin.py
+++ apps/reviewer_assignment/admin.py
@@ -5,6 +5,7 @@ from django.utils.html import format_html
 
 from .models import *
 from .forms import AssignmentForm
+from apps.article_review.models import Review
 
 
 class AssignmentAdmin(admin.ModelAdmin):
@@ -18,19 +19,43 @@ class AssignmentAdmin(admin.ModelAdmin):
              p.user.get_full_name() + '</a>' for p in obj.referees.all()]))
     get_referees.short_description = 'Árbitros'
 
+    def get_profiles(self, obj):
+        return format_html('<br>'.join(
+            ['<a href="/admin/Asignacion_Arbitros/profile/' + str(p.id) + '/change/">' + str(p) + '</a>' for p in obj.article.profile.profiles.all()]))
+
+    get_profiles.short_description = 'Perfiles'
+
     list_display = ('article', 'get_referees', 'status')
 
-    readonly_fields = ('article', 'publication')
+    readonly_fields = ('article', 'publication', 'get_profiles')
 
     def message_user(self, *args, **kwargs):
         pass
 
     def save_model(self, request, obj, form, change):
         if change:
+
+            if 'referees' in form.changed_data and form.cleaned_data['referees'].count() > 0 or obj.referees.count() > 0:
+                obj.status = 'A'
+
+                Review.objects.get_or_create(
+                    assignment=obj,
+                )
+
+            else:
+                obj.status = 'P'
+                Review.objects.filter(assignment=obj).delete()
             messages.success(
                 request,
                 format_html(
                     'La asignación del artículo <a>' + obj.article.title + '</a> se ha actualizado correctamente'))
+
+            if 'referees' in form.changed_data and form.cleaned_data['referees'].count() == 0:
+                obj.status = 'P'
+                messages.warning(
+                    request,
+                    format_html(
+                        'La asignación del artículo <a>' + obj.article.title + '</a> no tiene árbitros asignados'))
         else:
             messages.success(request,
                              format_html(
@@ -44,11 +69,48 @@ class AssignmentAdmin(admin.ModelAdmin):
                              'La asignación del artículo' + obj.article.title + ' se ha eliminado correctamente'))
         obj.delete()
 
+
 class ArticleProfileAdmin(admin.ModelAdmin):
-    
-    readonly_fields = ('article', 'publication')
-    
-    
+
+    readonly_fields = ('article', 'publication', 'status')
+
+    def get_perfiles_article(self, obj):
+        return format_html('<br>'.join(
+            ['<a href="/admin/Asignacion_Arbitros/profile/' + str(p.id) + '/change/">' + str(p) + '</a>' for p in obj.profiles.all()]))
+
+    get_perfiles_article.short_description = 'Perfiles'
+
+    list_display = ('article', 'get_perfiles_article', 'status')
+
+    list_filter = ('status', 'profiles')
+
+    search_fields = ('article__title',
+                     'profiles__user__first_name', 'profiles__user__last_name')
+
+    def message_user(self, *args, **kwargs):
+        pass
+
+    # * create assigment for each article when update and status is 2
+    def save_model(self, request, obj, form, change):
+        if change:
+            if 'profiles' in form.changed_data and form.cleaned_data['profiles'].count() > 0:
+
+                Assignment.objects.get_or_create(
+                    article=obj.article,
+                    publication=obj.publication,
+                )
+                obj.status = 'A'
+                obj.save()
+            else:
+                obj.status = 'P'
+                obj.save()
+            messages.success(request, format_html(
+                'El artículo <a>' + obj.article.title + '</a> se ha actualizado correctamente'))
+        else:
+
+            messages.success(request, format_html(
+                'El artículo <a>' + obj.article.title + '</a> se ha creado correctamente'))
+
 
 admin.site.register(Assignment, AssignmentAdmin)
 
