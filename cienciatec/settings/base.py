7cd34e79d9a341ef3371fb3d2f9a845bc090c197
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-03-03 03:39:39 -0600

    cabinet app and no base meail settings

--- cienciatec/settings/base.py
+++ cienciatec/settings/base.py
@@ -58,6 +58,8 @@ DJANGO_APPS = [
 ]
 
 THIRD_PARTY_APPS = [
+    'cabinet',
+    'imagefield',
     'jet.dashboard',
     'jet',
     # 'daphne',
@@ -151,14 +153,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# * Email
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 # * Logout
