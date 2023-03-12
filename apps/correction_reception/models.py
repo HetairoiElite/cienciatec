c41c721135c7b04ca7399583a02f4b138ef796ff
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-03-12 08:23:17 -0600

    model form correction reception

--- apps/correction_reception/models.py
+++ apps/correction_reception/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from model_utils.models import TimeStampedModel
 # Create your models here.
+
+
+def custom_upload__to_correction(instance, filename):
+    try:
+
+        old_instance = ArticleCorrection.objects.get(pk=instance.pk)
+        old_instance.correction_file.delete()
+        return 'corrections/' + filename
+    except:
+        return 'corrections/' + filename
+
+
+class ArticleCorrection(TimeStampedModel):
+    correction_file = models.FileField(
+        upload_to=custom_upload__to_correction, verbose_name='Plantilla corregida', null=True)
+
+    article = models.OneToOneField(
+        'Recepcion_Propuestas.ArticleProposal', on_delete=models.CASCADE, related_name='correction', verbose_name='Artículo', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Corrección'
+        verbose_name_plural = 'Correcciones'
+
+    def __str__(self):
+        return str(self.article)
