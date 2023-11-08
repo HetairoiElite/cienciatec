c272c6cc0f1ad88fed18e4cc944d00107c7439b6
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-11-08 09:19:40 -0600

    DictamenView

--- apps/correction_reception/urls.py
+++ apps/correction_reception/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import CorrectionFormView
+from .views import CorrectionFormView, DictamenView
 
 app_name = 'correction_reception'
 
 urlpatterns = [
     path('<int:pk>/', CorrectionFormView.as_view(), name='correction_form'),
+    path('dictamen/<int:pk>/', DictamenView.as_view(), name='dictamen'),
+    
 ]
