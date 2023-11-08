e87d78806d17c880e24d43f60ae509a33a4fa979
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-11-08 08:48:53 -0600

    Dispatch tablero

--- apps/core_dashboard/views.py
+++ apps/core_dashboard/views.py
@@ -11,12 +11,24 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # from apps.chat.models import Thread
 
 
+from core.models import Home
 # Create your views here.
+from django.contrib import messages
 
-
+from django.shortcuts import redirect
 class Dashboard(LoginRequiredMixin, TemplateView):
     template_name = 'core_dashboard/index.html'
+    
 
+    def dispatch(self, request, *args, **kwargs):
+        publicacion = Home.objects.first().publication
+        
+        if not publicacion:
+            # * mensaje
+            messages.error(
+                request, 'No hay un periodo de publicación disponible.')
+            return redirect('core:home')
 
+        return super().dispatch(request, *args, **kwargs)
 # class ProposalArticleDetail(LoginRequiredMixin, TemplateView):
 #     template_name = 'proposal_reception/article_proposal_detail.html'
