b447714f1360e4389488504f77b0521c3c3dd010
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-11-08 09:19:03 -0600

    correction file

--- apps/correction_reception/models.py
+++ apps/correction_reception/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
 # Create your models here.
+from django.conf import settings
 
 
 def custom_upload__to_correction(instance, filename):
@@ -12,10 +13,21 @@ def custom_upload__to_correction(instance, filename):
     except:
         return 'corrections/' + filename
 
+def custom_upload__to_correction_as_pdf(instance, filename):
+    try:
+
+        old_instance = ArticleCorrection.objects.get(pk=instance.pk)
+        old_instance.correction_file_as_pdf.delete()
+        return 'corrections/' + filename
+    except:
+        return 'corrections/' + filename
 
 class ArticleCorrection(TimeStampedModel):
     correction_file = models.FileField(
-        upload_to=custom_upload__to_correction, verbose_name='Plantilla corregida', null=True)
+        upload_to=custom_upload__to_correction, verbose_name='Plantilla corregida', null=True, max_length=255)
+    
+    correction_file_as_pdf = models.FileField(
+        upload_to=custom_upload__to_correction, verbose_name='Plantilla corregida en PDF', null=True, max_length=255)
 
     article = models.OneToOneField(
         'Recepcion_Propuestas.ArticleProposal', on_delete=models.CASCADE, related_name='correction', verbose_name='Artículo', null=True, blank=True)
@@ -26,3 +38,56 @@ class ArticleCorrection(TimeStampedModel):
 
     def __str__(self):
         return str(self.article)
+    
+    # * correction file as pdf
+    
+    def generate_correction_file_as_pdf(self):
+        from dotenv import load_dotenv
+        import os
+
+        load_dotenv()
+
+        DJANGO_SETTINGS_MODULE = os.getenv('DJANGO_SETTINGS_MODULE')
+
+        if DJANGO_SETTINGS_MODULE == 'cienciatec.settings.local':
+            from docx2pdf import convert
+            import pythoncom
+            import shutil
+
+            pythoncom.CoInitialize()
+            
+            # * delete old correction_file to downloads folder
+            
+            try:
+                os.remove(settings.BASE_DIR / 'downloads' / 'correction_file.docx')
+            except:
+                pass
+
+            # * copy correction_file to downloads folder
+            shutil.copy(self.correction_file.path, settings.BASE_DIR /
+                        'downloads' / 'correction_file.docx')
+
+            convert(settings.BASE_DIR / 'downloads' / 'correction_file.docx',
+                    settings.BASE_DIR / 'downloads' / 'correction_file.pdf')
+
+        else:
+            import subprocess
+            import boto3
+            # * download template from s3
+            s3 = boto3.resource('s3')
+            path = self.correction_file.path
+
+            bucket = s3.Bucket(os.getenv('AWS_STORAGE_BUCKET_NAME'))
+            bucket.download_file(path, settings.BASE_DIR /
+                                 'downloads' / 'correction_file.docx')
+
+            output = subprocess.check_output(('libreoffice', '--headless', '--convert-to', 'pdf',
+                                              settings.BASE_DIR / 'downloads' / 'correction_file.docx',  '--outdir', settings.BASE_DIR / 'downloads'))
+
+        with open(settings.BASE_DIR / 'downloads' / 'correction_file.pdf', 'rb') as f:
+            from django.core.files import File
+            self.correction_file_as_pdf.save(f'{self.article.title}-corregido.pdf',
+                                      File(f))
+
+        self.save()   
+        
