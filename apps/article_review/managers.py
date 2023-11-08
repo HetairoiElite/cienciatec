3cc0c1ad7e2128526ec0b34b1518194e11f515df
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-11-08 09:14:31 -0600

    managers para dictamenes

--- apps/article_review/managers.py
+++ apps/article_review/managers.py
@@ -13,3 +13,28 @@ class ReviewManager(Manager):
             return review[0].enviado
         except:
             return False
+
+    def has_notes(self, assignment, user):
+        notes = self.filter(assignment=assignment,
+                            referee=user.profile)[0].notes.all().count()
+
+        if notes > 0:
+            return True
+        else:
+            return False
+
+    def get_reported_reviews(self, assignment):
+        # * count enviado = True reviews
+        from django.db.models import Q
+        return self.filter(Q(dictamen='1') | Q(dictamen='2'), assignment=assignment).count()
+
+    def get_is_reported(self, assignment, user):
+        # * check if review is sent
+        try:
+            review = self.filter(assignment=assignment, referee=user.profile)
+            if review[0].dictamen == '1' or review[0].dictamen == '2':
+                return True
+            else:
+                return False
+        except:
+            return False
