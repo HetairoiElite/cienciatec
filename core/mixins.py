f82d46c69bd48ae8b3a692e3e870398db0f8c67e
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-11-08 10:53:12 -0600

    mixin notadmincheckpublication

---
+++ core/mixins.py
@@ -0,0 +1,22 @@
+# * Mixins
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.shortcuts import redirect
+from apps.events.models import Publication
+
+class NotAdminCheckPublicationMixin(LoginRequiredMixin):
+    
+    def dispatch(self, request, *args, **kwargs):
+        publicacion = Publication.objects.get_current()
+        
+        # * si el usuario es un autor o un editor
+        if request.user.profile.type_user == '1' or request.user.profile.type_user == '2':
+            if not publicacion:
+                # * mensaje
+                messages.error(
+                    request, 'No hay un periodo de publicación disponible.')
+                return redirect('home')
+        else:
+            return redirect('home')
+        
+        return super().dispatch(request, *args, **kwargs)
