1af6464f754b2e12cb38342594032fb199654304
HetairoiElite <jonathan90090@gmail.com>
2023-11-22 22:36:58 +0000

    limits dates

--- apps/core_dashboard/views.py
+++ apps/core_dashboard/views.py
@@ -20,10 +20,14 @@ class Dashboard(LoginRequiredMixin, TemplateView):
     template_name = 'core_dashboard/index.html'
     
 
+    from django.utils import timezone
+
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
         publicacion = Publication.objects.get_current()
-        
+        from django.utils import timezone
+        now = timezone.now()
+
         # * si el usuario es un autor o un editor
         if request.user.profile.type_user == '1' or request.user.profile.type_user == '2':
             if not publicacion:
@@ -31,8 +35,15 @@ class Dashboard(LoginRequiredMixin, TemplateView):
                 messages.error(
                     request, 'No hay un periodo de publicación disponible.')
                 return redirect('home')
+            elif not (publicacion.start_date <= now <= publicacion.end_date):
+                messages.error(
+                    request, 'Actualmente no estamos dentro de un periodo de publicación o el periodo de publicación puede haber terminado. Por favor, regrese más tarde cuando un nuevo periodo de publicación esté activo.')
+                return redirect('home')
         else:
             return redirect('home')
+        
+        # if request.user.profile.type_user == '2':
+            
 
         return super().dispatch(request, *args, **kwargs)
 # class ProposalArticleDetail(LoginRequiredMixin, TemplateView):
