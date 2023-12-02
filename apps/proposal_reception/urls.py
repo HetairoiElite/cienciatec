9c6875c77145c1de89392a1422d16ddc7e9df267
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-12-02 06:29:00 -0600

    remove test

--- apps/proposal_reception/urls.py
+++ apps/proposal_reception/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('crear/', views.ProposalFormView.as_view() , name='article_proposal_form'),
     path('<int:pk>/edit/', views.ArticleProposalUpdateView.as_view(), name='article_proposal_update'),
     path('check-title/', views.CheckTitleView.as_view(), name='check_title'),
+    
     # path('prueba/', views.prueba, name='prueba')
 ]
