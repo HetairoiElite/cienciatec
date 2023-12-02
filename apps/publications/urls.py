c764772bb7f0b7758f9215503befddc5ba9a2ee0
Jonathan Osorio Sánchez <jonathan90090@gmail.com>
2023-12-02 06:31:09 -0600

    article view

--- apps/publications/urls.py
+++ apps/publications/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 # * app_name
 
-app_name = 'events'
+app_name = 'publications'
 
 urlpatterns = [
     path('', views.CalendarView.as_view(), name='calendar'),
+    path('articulo/<int:pk>/', views.ArticleView.as_view(), name='article_detail'),
+    
 ]
