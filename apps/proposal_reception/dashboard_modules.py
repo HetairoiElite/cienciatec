af1be2a9ec50b89701c4114449c75ab9bec7b034
HetairoiElite <jonathan90090@gmail.com>
2024-08-20 13:13:21 -0600

    division por zero

--- apps/proposal_reception/dashboard_modules.py
+++ apps/proposal_reception/dashboard_modules.py
@@ -10,24 +10,9 @@ class RecentArticleProposals(DashboardModule):
 
     def init_with_context(self, context):
         current_pub = Publication.objects.get_current()
-        
+
         if current_pub:
-            
             articles = ArticleProposal.objects.get_articles_by_pub(current_pub)
-            
-            por_recibir = articles.filter(status='1').count()
-            por_asignar = articles.filter(status='2').count()
-            en_revision = articles.filter(status='3').count()
-            por_corregir = articles.filter(status='4').count()
-            por_recibir_correcciones = articles.filter(status='5').count()
-            por_dictaminar = articles.filter(status='6').count()
-            en_dictamen = articles.filter(status='7').count()
-            from django.db.models import Q
-            aceptados = articles.filter(
-                Q(status='8') | Q(status='10') | Q(status='11')).distinct().count()
-            rechazados = articles.filter(status='9').count()
-            pendientes = articles.filter(status='10').count()
-            por_publicar = articles.filter(status='11').count()
 
             self.children.append(
                 {
@@ -35,71 +20,89 @@ class RecentArticleProposals(DashboardModule):
                     'value': '#' + str(current_pub.numero_publicacion)
                 }
             )
-            self.children.append({
-                'title': 'Por recibir',
-                'value': f'{por_recibir} - {por_recibir * 100 / articles.count():.2f}%',
-            })
 
-            self.children.append({
-                'title': 'Por asignar',
-                'value': f'{por_asignar} - {por_asignar * 100 / articles.count():.2f}%',
-            })
-            
-            self.children.append({
-                'title': 'En revisión',
-                'value': f'{en_revision} - {en_revision * 100 / articles.count():.2f}%',
-            })
-            
-            self.children.append({
-                
-                'title': 'Por corregir',
-                'value': f'{por_corregir} - {por_corregir * 100 / articles.count():.2f}%',
-            })
-            
-            self.children.append({
-                
-                'title': 'Por recibir correcciones',
-                'value': f'{por_recibir_correcciones} - {por_recibir_correcciones * 100 / articles.count():.2f}%',
-            })
-            
-            self.children.append({
-                'title': 'Por dictaminar',
-                'value': f'{por_dictaminar} - {por_dictaminar * 100 / articles.count():.2f}%',
-            })
-            
-            self.children.append({
-                'title': 'En dictamen',
-                'value': f'{en_dictamen} - {en_dictamen * 100 / articles.count():.2f}%',
-            })
-            
-            self.children.append({
-                'title': 'Aceptados',
-                'value': f'{aceptados} - {aceptados * 100 / articles.count():.2f}%',
-            })
-            
-            self.children.append({
-                'title': 'Rechazados',
-                'value': f'{rechazados} - {rechazados * 100 / articles.count():.2f}%',
-            })
-            
-            self.children.append({
-                'title': 'Pendientes',
-                'value': f'{pendientes} - {pendientes * 100 / articles.count():.2f}%',
-            })
-            
-            self.children.append({ 
-                'title': 'Publicados',
-                'value': f'{por_publicar} - {por_publicar * 100 / articles.count():.2f}%',
-            })
-            
-            self.children.append({
-                'title': 'Total',
-                'value': f'{articles.count()}',
-            })
+            if articles.count() != 0:
+                por_recibir = articles.filter(status='1').count()
+                por_asignar = articles.filter(status='2').count()
+                en_revision = articles.filter(status='3').count()
+                por_corregir = articles.filter(status='4').count()
+                por_recibir_correcciones = articles.filter(status='5').count()
+                por_dictaminar = articles.filter(status='6').count()
+                en_dictamen = articles.filter(status='7').count()
+
+                from django.db.models import Q
+                aceptados = articles.filter(
+                    Q(status='8') | Q(status='10') | Q(status='11')).distinct().count()
+                rechazados = articles.filter(status='9').count()
+                pendientes = articles.filter(status='10').count()
+                por_publicar = articles.filter(status='11').count()
+
+                self.children.append({
+                    'title': 'Por recibir',
+                    'value': f'{por_recibir} - {por_recibir * 100 / articles.count():.2f}%',
+                })
+
+                self.children.append({
+                    'title': 'Por asignar',
+                    'value': f'{por_asignar} - {por_asignar * 100 / articles.count():.2f}%',
+                })
+
+                self.children.append({
+                    'title': 'En revisión',
+                    'value': f'{en_revision} - {en_revision * 100 / articles.count():.2f}%',
+                })
+
+                self.children.append({
+                    'title': 'Por corregir',
+                    'value': f'{por_corregir} - {por_corregir * 100 / articles.count():.2f}%',
+                })
+
+                self.children.append({
+                    'title': 'Por recibir correcciones',
+                    'value': f'{por_recibir_correcciones} - {por_recibir_correcciones * 100 / articles.count():.2f}%',
+                })
+
+                self.children.append({
+                    'title': 'Por dictaminar',
+                    'value': f'{por_dictaminar} - {por_dictaminar * 100 / articles.count():.2f}%',
+                })
+
+                self.children.append({
+                    'title': 'En dictamen',
+                    'value': f'{en_dictamen} - {en_dictamen * 100 / articles.count():.2f}%',
+                })
+
+                self.children.append({
+                    'title': 'Aceptados',
+                    'value': f'{aceptados} - {aceptados * 100 / articles.count():.2f}%',
+                })
+
+                self.children.append({
+                    'title': 'Rechazados',
+                    'value': f'{rechazados} - {rechazados * 100 / articles.count():.2f}%',
+                })
+
+                self.children.append({
+                    'title': 'Pendientes',
+                    'value': f'{pendientes} - {pendientes * 100 / articles.count():.2f}%',
+                })
+
+                self.children.append({
+                    'title': 'Publicados',
+                    'value': f'{por_publicar} - {por_publicar * 100 / articles.count():.2f}%',
+                })
+
+                self.children.append({
+                    'title': 'Total',
+                    'value': f'{articles.count()}',
+                })
+            else:
+                self.children.append({
+                    'title': 'No hay propuestas de artículo.',
+                    'value': 'Nada que mostrar'
+                })
         else:
             self.children.append({
                 'title': 'No hay periodo de publicación activo.',
-                'value' :'Nada que mostrar'
+                'value': 'Nada que mostrar'
             })
-            
-            
