65fbabc93e18aa8018abd91e7a5b59680cd29038
HetairoiElite <jonathan90090@gmail.com>
2024-08-22 12:21:48 -0600

    correos

--- cienciatec/settings/prod.py
+++ cienciatec/settings/prod.py
@@ -74,7 +74,7 @@ DATABASES = {
 # * Email
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-mail.outlook.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
